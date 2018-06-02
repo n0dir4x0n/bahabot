23e332270a3ef21240e07551132eee9c002ca04b
AchilovJavohir <javoxiraka@gmail.com>
2018-06-02 16:12:24 +0500

    feat(votesmigration)

--- migrations/20180522092637_votes.js
+++ migrations/20180522092637_votes.js
@@ -3,13 +3,18 @@ exports.up = function (knex, Promise) {
         knex.schema.createTable('votes', function (table) {
             table.increments();
             table.string('username');
+            table.string('userid');
             table.string('specialgroup');
+            table.string('specialgroupid');
             table.text('channel');
-            table.text('channeluserscount');
-            table.string('link');
-            table.string('votetext');
+            table.text('channelid');
+            table.integer('chucbefore');    //channel users count before
+            table.integer('chucafter');     //channel users count after
+            table.text('link');
+            table.text('votetext');
             table.string('vip');
             table.boolean('isactive');
+            table.timestamps();
          }),
 
     ])
